a3a5402240bd4215f30ccb7ab6333cafda4cc143
jhtory <mineship@pusan.ac.kr>
2024-03-06 18:41:42 +0900

    본문 삽입까지 함, 조건부 렌더링 아직안함

--- src/components/home/NoticeCarousel.tsx
+++ src/components/home/NoticeCarousel.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/carousel_notice";
 import Autoplay from "embla-carousel-autoplay";
 import { IoAdd } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 export interface NoticeData {
   id: string;
@@ -36,7 +37,7 @@ const NoticeCarousel = ({ dataArray }: Props) => {
     >
       {/* 캐러셀 헤더 */}
       <div className="self-stretch flex flex-row items-center justify-between">
-        <div className="text-[16px] font-['Noto_Sans_KR'] font-medium text-[#000] whitespace-nowrap">
+        <div className="text-[16px] font-['Noto_Sans_KR'] font-bold gradient-primary whitespace-nowrap">
           NOTICE
         </div>
         {/* 버튼 래퍼 */}
@@ -46,7 +47,9 @@ const NoticeCarousel = ({ dataArray }: Props) => {
             <div className="w-[1px] h-[12px] shrink-0 bg-[#ddd] rounded-[16px]"></div>
             <CarouselNext />
           </div>
-          <IoAdd className="h-4 w-4" />
+          <Link to="notice">
+            <IoAdd className="h-4 w-4" />
+          </Link>
         </div>
       </div>
       {/* 캐러셀 콘텐츠 */}
@@ -55,9 +58,15 @@ const NoticeCarousel = ({ dataArray }: Props) => {
           return (
             <CarouselItem key={data.id}>
               <div className="self-stretch h-[77px] lg:h-[322px] shrink-0 flex flex-col items-start justify-between">
-                <div className="self-stretch text-[16px] font-['Noto_Sans_KR'] text-[#000] line-clamp-2">
-                  {data.title}
+                <div className="self-stretch flex flex-col items-start justify-start gap-[24px]">
+                  <div className="self-stretch text-[16px] font-['Noto_Sans_KR'] font-medium text-[#000] line-clamp-2">
+                    {data.title}
+                  </div>
+                  <div className="self-stretch text-[16px] font-['Noto_Sans_KR'] text-[#000] line-clamp-6">
+                    {data.content}
+                  </div>
                 </div>
+
                 <div className="text-[16px] font-['Noto_Sans_KR'] text-[#57585a] whitespace-nowrap">
                   {data.date}
                 </div>
